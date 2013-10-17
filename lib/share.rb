ed1bda91c7a528eff51ddcdb7f0d1d5a38b59409
onionjake <onion_jake@hotmail.com>
2013-10-17 00:58:17 -0600

    Create preshared keys and private keys.

--- lib/share.rb
+++ lib/share.rb
@@ -6,28 +6,69 @@ require 'share/file'
 require 'permahash'
 require 'securerandom'
 require 'conf'
+require 'openssl'
+require 'digest'
 
 class Share
   attr_reader :id, :peer_id
 
   def initialize share_id
-    @id = share_id
     path = "#{Conf.data_dir}/share_#{share_id}.db"
     @db = Permahash.new path
 
     @by_sha = {}
     self.each { |path,file| @by_sha[file.sha256] = file }
 
-    @peer_id = @db[:peer_id] ||= SecureRandom.hex(32)
-    @db[:codes] ||= []
+    # Peer_id is always unique, so it can be created here.  Unlike the psks and 
+    # the pkeys.
+    @db[:peer_id] ||= SecureRandom.hex(16)
+    @db[:codes]   ||= []
 
     @db.flush
   end
 
+  # Make sure values in @db look sane. Helpful when loading from disk
+  # or creating over the network.
+  def validate_share
+    #TODO
+  end
+
+  def check_key_type_and_level type,level
+    raise "Invalid key type #{type}." unless [:pkey, :psk].include? type
+    raise "Invalid access level" unless [:read_write, :read_only, :untrusted].include? level
+  end
+
+  def set_key type, level, key
+    check_key_type_and_level type,level
+
+    @db[type] ||= {} 
+    @db[type][level] = key
+
+    #force a save
+    @db[type] = @db[type]
+  end
+
+  def key type, level
+    check_key_type_and_level type,level
+
+    @db[type][level]
+  end
+
   def self.create path
-    share_id = SecureRandom.hex 16
+    psks = {
+      :read_write => SecureRandom.hex(16),
+      :read_only  => SecureRandom.hex(16),
+      :untrusted  => SecureRandom.hex(16),
+    }
+    pkeys = {
+      :read_write => OpenSSL::PKey::RSA.new(4096).to_s,
+      :read_only  => OpenSSL::PKey::RSA.new(4096).to_s,
+    }
+    share_id = Digest::SHA256.hexdigest(psks[:read_write])
     share = Share.new share_id
     share.path = path
+    psks.each  { |k,v| share.set_key  :psk, k, v }
+    pkeys.each { |k,v| share.set_key :pkey, k, v }
     share
   end
 
@@ -37,11 +78,6 @@ class Share
 
   def path
     @db[:path]
-  end
-
-  def key level
-    # FIXME
-    "abcdef"
   end
 
   def access_code
