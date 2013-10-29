ae151594fc75466a12974ecc6de04e9e612b62ee
Jewel <github@thesteve.org>
2013-10-28 21:32:59 -0600

    Add class documentation

--- lib/access_code.rb
+++ lib/access_code.rb
@@ -1,22 +1,35 @@
 # Class to represent a single access code.
+#
+# See protocol/core.md for an explanation of access codes.
 
 require 'base32'
 require 'luhn_check'
 require 'digest/sha2'
 require 'securerandom'
 
+# This class converts the access code to a textual representation and can also
+# parse that representation.
 class AccessCode
+
+  # Create an AccessCode object from existing key material
+  #
+  # payload  -  a binary string containing the access code's key
   def initialize payload
     @payload = payload
   end
 
+  # Create a new Access Code object
   def self.create
     payload = SecureRandom.random_bytes 7
     self.new payload
   end
 
+  # Parse an access code in ASCII format.
+  # These are BASE32 encoded.
   def self.parse str
     raise "Wrong length, should be 17 characters, not #{str.size} characters" unless str.size == 17
+
+    str.upcase!
 
     raise "Missing 'SYNC' prefix" unless str =~ /\ASYNC/
 
@@ -34,19 +47,29 @@ class AccessCode
     self.new payload
   end
 
+  # Get the ID of the access code.  This is similar to the "Share ID" of a
+  # share, and is used to locate other nodes that have the same access code.
   def id
     @id ||= Digest::SHA256.hexdigest(@payload)
   end
 
+  # Get base32 representation of the access code, for sharing with other
+  # people.
   def to_s
     LuhnCheck.generate(Base32.encode("\x96\x1A\x2B" + @payload))
   end
 
+  # Get the key material for the access code.  This asks for the desired
+  # access_level so that it behaves in a similar way to the Share class, but
+  # the access_level of an AccessCode is always "unknown".
   def key access_level
     raise "Invalid access level" unless access_level == :unknown
     @payload
   end
 
+  # Get the access_level of the Access Code.  This is always "unknown", since
+  # access codes don't contain any information about the access level they will
+  # represent.
   def access_level
     :unknown
   end
--- lib/base32.rb
+++ lib/base32.rb
@@ -1,8 +1,13 @@
 # This is a simple base32 implementation
 #
 # It is not efficient (as it doesn't need to be for ClearSkies usage)
+#
+# See RFC 4648.
 
 module Base32
+
+  # Encode a binary string into Base32 ASCII representation.  Note that for
+  # clearskies, only strings with lengths divisible by 5 are needed.
   def self.encode str
     if str.size % 5 != 0
       raise "Base32 needs a string length divisible by 5"
@@ -22,6 +27,8 @@ module Base32
     characters.map(&:chr).join ''
   end
 
+  # Get the numeric value for a character.  Here we do the non-standard
+  # substitutions of "O" for "0" and "L" for "1" to help confused users.
   def self.ord char
     # Replace 0 with O and 1 with L
     char = 'O' if char == '0'
@@ -36,6 +43,7 @@ module Base32
     end
   end
 
+  # Get the character that an integer value should encode to.
   def self.chr val
     if val >= 0 && val < 26
       ('A'.ord + val).chr
@@ -46,6 +54,7 @@ module Base32
     end
   end
 
+  # Decode a Base32 ASCII string into a binary string
   def self.decode str
     str = str.upcase
     res = String.new
